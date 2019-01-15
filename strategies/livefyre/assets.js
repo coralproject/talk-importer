7691ca75b1a4ce069a683b7981431445f78cee02
immber <jessiekrushing@gmail.com>
2019-01-14 16:01:10 -0800

    fixed bug in assets import

--- strategies/livefyre/assets.js
+++ strategies/livefyre/assets.js
@@ -1,3 +1,4 @@
+const h = require('highland');
 module.exports = {
   /**
    * Turn the collection into a Talk asset
@@ -6,11 +7,12 @@ module.exports = {
    * @return {Object}
    */
   translate: fyre => {
-    return {
+    var asset = {
       id: fyre.id,
       url: fyre.source, // This url needs to be added in the permitted domains section of your Talk admin
       title: fyre.title,
       scraped: null, // Set to null because next visit to page will trigger scrape
     };
+    return h([asset]);
   },
 };
