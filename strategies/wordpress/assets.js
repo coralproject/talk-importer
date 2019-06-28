f618ddfbebf7f828a6585aaf994f98c2479d1b97
jessie.rushing <jessie.rushing@voxmedia.com>
2019-06-28 15:33:38 -0700

    fixed asset url string

--- strategies/wordpress/assets.js
+++ strategies/wordpress/assets.js
@@ -9,7 +9,7 @@ module.exports = {
   translate: fyre => {
     var asset = {
       id: fyre.comment_post_ID,
-      url: `https://rivardreport.com/asset/${fyre.comment_post_ID}`, // This url needs to be added in the permitted domains section of your Talk admin
+      url: `https://yourdomainhere.com/asset/${fyre.comment_post_ID}`, // This url needs to be added in the permitted domains section of your Talk admin
       title: fyre.comment_post_ID,
       scraped: null, // Set to null because next visit to page will trigger scrape
     };
